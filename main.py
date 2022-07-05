601182e70ca01c2f04c005a42d3ebff2a5754395
Alexandre Samson <alexander_samson@icloud.com>
2022-07-05 18:38:13 +0200

    2022.07.05

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hello Alex!")
+
+x = " Как дела?"
+print("Hello Alex! ", x)
+
+greeting = "Hello!"
+first_name = "Jack"
+last_name = 'White'
+exclamation_sign = "!"
+white_space = " "
+
+print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
+
+long_string = "This ist the long string"
+print(long_string)
+another_string = 'Я хочу учить язык "Python"'
+string_aaa = 'I\'m a programmer'
+print(string_aaa)
+string_with_new_lines = "Hello! \n My name is Alex"
+print(string_with_new_lines)
+numbers = "1\t23\t4567"
+print(numbers)
+
+#Triple quotes
+
+string_with_triple_quotes = """This is ' text with "triple quotes" """
+print(string_with_triple_quotes)
